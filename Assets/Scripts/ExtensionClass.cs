458196a3e2487795a3384af8a4b5e4683c263c90
Thompson Lee <tom.mai78101@gmail.com>
2015-10-05 01:50:07 -0400

    Just adding classes.

---
+++ Assets/Scripts/ExtensionClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Extension {
+	public static class ExtensionClass {
+		public static bool ReachedDestination(this NavMeshAgent agent) {
+			if (!agent.pathPending) {
+				if (agent.stoppingDistance > 0f) {
+					if (agent.remainingDistance <= agent.stoppingDistance) {
+						if (!agent.hasPath || agent.velocity.sqrMagnitude <= float.Epsilon) {
+							return true;
+						}
+					}
+				}
+				else {
+					if (!agent.hasPath || agent.velocity.sqrMagnitude <= float.Epsilon) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LineOfSight : MonoBehaviour {
+	public bool enemySpotted;
+	public List<GameObject> enemiesInRange;
+
+	public void OnTriggerEnter(Collider other) {
+		//if (other.tag.Equals("Unit")) {
+		//	if (this.enemiesInRange.Count <= 0) {
+		//		this.enemySpotted = true;
+		//	}
+		//	GameUnit otherUnit = other.GetComponent<GameUnit>();
+		//	if (!this.enemiesInRange.Contains(other.gameObject) && !otherUnit.hasAuthority) {
+		//		this.enemiesInRange.Add(other.gameObject);
+		//	}
+		//}
+	}
+
+	public void OnTriggerExit(Collider other) {
+		//if (other.tag.Equals("Unit")) {
+		//	GameUnit otherUnit = other.GetComponent<GameUnit>();
+		//	if (this.enemiesInRange.Contains(other.gameObject) && !otherUnit.hasAuthority) {
+		//		this.enemiesInRange.Remove(other.gameObject);
+		//	}
+		//	if (this.enemiesInRange.Count <= 0) {
+		//		this.enemySpotted = false;
+		//	}
+		//}
+	}
+}
