8603d92ccde6bedccbbb01cb7bac4704a769ca6d
Thompson Lee <tom.mai78101@gmail.com>
2015-10-05 22:24:36 -0400

    Adding Attack Area.

---
+++ Assets/Scripts/AttackArea.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AttackArea : MonoBehaviour {
+	//Same design pattern as LineOfSight class.
+	public List<GameUnit> enemies = new List<GameUnit>();
+
+	public void OnTriggerEnter(Collider other) {
+		GameUnit unit = other.GetComponent<GameUnit>();
+		if (unit != null && !unit.hasAuthority && !this.enemies.Contains(unit)) {
+			this.enemies.Add(unit);
+		}
+	}
+
+	public void OnTriggerExit(Collider other) {
+		GameUnit unit = other.GetComponent<GameUnit>();
+		if (unit != null && !unit.hasAuthority && this.enemies.Contains(unit)) {
+			this.enemies.Remove(unit);
+		}
+	}
+}
