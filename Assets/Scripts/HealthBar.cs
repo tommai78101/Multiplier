11713598d23056a5725c546a5f2b525fd77de601
Thompson Lee <tom.mai78101@gmail.com>
2015-10-14 18:32:12 -0400

    Added health bar to the game unit.

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class HealthBar : MonoBehaviour {
+	public GameUnit unit;
+
+	public void OnGUI() {
+		GUIStyle style = new GUIStyle();
+		style.normal.textColor = Color.black;
+		style.alignment = TextAnchor.MiddleCenter;
+		Vector3 healthPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+		Rect healthRect = new Rect(healthPosition.x - 50f, (Screen.height - healthPosition.y) - 45f, 100f, 25f);
+		GUI.Label(healthRect, unit.currentHealth.ToString() + "/" + unit.maxHealth.ToString(), style);
+	}
+}
