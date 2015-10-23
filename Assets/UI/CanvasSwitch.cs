d2b65ecced5fd1a9e513fd3986acb2c1791374d8
Thompson Lee <tom.mai78101@gmail.com>
2015-10-22 20:56:06 -0400

    Allow UI menu to show/hide by pressing M key.

---
+++ Assets/UI/CanvasSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasSwitch : MonoBehaviour {
+	public CanvasGroup canvasGroup;
+	public bool showCanvas;
+
+	public void Start() {
+		if (this.canvasGroup == null) {
+			Debug.LogError("Canvas group has not been set. Please check.");
+		}
+		this.showCanvas = true;
+		this.ToggleCanvas();
+	}
+
+	public void Update() {
+		if (Input.GetKeyUp(KeyCode.M)) {
+			this.showCanvas = !this.showCanvas;
+			this.ToggleCanvas();
+		}
+	}
+
+	public void ToggleCanvas() {
+		this.canvasGroup.alpha = this.showCanvas ? 1f : 0f;
+		this.canvasGroup.interactable = this.showCanvas;
+		this.canvasGroup.blocksRaycasts = this.showCanvas;
+	}
+}
