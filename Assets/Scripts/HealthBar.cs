690fdf51dd1ed59c97b66e14b067719e308496c1
Thompson Lee <tom.mai78101@gmail.com>
2015-11-21 04:34:40 -0500

    Another way of fixing it without using the cumbersome new Unity UI.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 
 public class HealthBar : MonoBehaviour {
 	public GameUnit unit;
+	public Camera minimapCamera;
+	public Vector3 viewportPosition;
+
+	public void Start() {
+		if (this.minimapCamera == null) {
+			GameObject obj = GameObject.FindGameObjectWithTag("Minimap");
+			this.minimapCamera = obj.GetComponent<Camera>();
+			if (this.minimapCamera == null) {
+				Debug.LogError("HealthBar: This failed to initialize minimap camera.");
+			}
+		}
+	}
 
 	public void OnGUI() {
 		GUIStyle style = new GUIStyle();
 		style.normal.textColor = Color.black;
 		style.alignment = TextAnchor.MiddleCenter;
 		Vector3 healthPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
-		Rect healthRect = new Rect(healthPosition.x - 50f, (Screen.height - healthPosition.y) - 45f, 100f, 25f);
-		GUI.Label(healthRect, unit.currentHealth.ToString() + "/" + unit.maxHealth.ToString(), style);
+		this.viewportPosition = Camera.main.ScreenToViewportPoint(new Vector3(healthPosition.x, healthPosition.y + 30f));
+		if (!this.minimapCamera.rect.Contains(this.viewportPosition)) {
+			Rect healthRect = new Rect(healthPosition.x - 50f, (Screen.height - healthPosition.y) - 45f, 100f, 25f);
+			GUI.Label(healthRect, unit.currentHealth.ToString() + "/" + unit.maxHealth.ToString(), style);
+		}
 	}
 }
