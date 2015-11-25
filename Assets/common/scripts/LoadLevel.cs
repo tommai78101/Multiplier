cc1d3160084c534caecd64b8a1401c11fddce3c0
Thompson Lee <tom.mai78101@gmail.com>
2015-11-25 00:24:53 -0500

    Moved LoadLevel script to Common. Added some buttons to single player UI.

---
+++ Assets/common/scripts/LoadLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Common {
+	public class LoadLevel : MonoBehaviour {
+		public void Load(string value) {
+			Application.LoadLevel(value);
+		}
+	}
+}
--- Assets/menu/LoadLevel.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LoadLevel : MonoBehaviour {
-	public void Load(string value) {
-		Application.LoadLevel(value);
-	}
-}
