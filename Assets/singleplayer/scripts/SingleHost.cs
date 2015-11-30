a5fadb707ef435e71a4f0f070b76aa8c0c3b0f41
Thompson Lee <tom.mai78101@gmail.com>
2015-11-29 22:15:49 -0500

    Trying to do a multiplayer with 1 player only.

---
+++ Assets/singleplayer/scripts/SingleHost.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class SingleHost : NetworkManager {
+	public CanvasGroup attributePanelGroup;
+
+	public void SingleStartHost() {
+		if (this.attributePanelGroup != null) {
+			this.attributePanelGroup.alpha = 0f;
+			this.attributePanelGroup.blocksRaycasts = false;
+			this.attributePanelGroup.interactable = false;
+		}
+
+		this.StartHost();
+	}
+
+}
