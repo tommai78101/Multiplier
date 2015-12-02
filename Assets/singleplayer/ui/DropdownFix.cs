9cd3cbe0204ee64c35f7dd17b892d12a61065f10
asperatology <tom.mai78101@gmail.com>
2015-12-01 22:01:43 -0500

    Tried a dropdown fix on the Unity UI. It sucks, but at least it "works" in such a way I would accept.

---
+++ Assets/singleplayer/ui/DropdownFix.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace SinglePlayer {
+	public class DropdownFix : Dropdown, IPointerClickHandler {
+		public override void OnPointerClick(PointerEventData eventData) {
+			base.OnPointerClick(eventData);
+
+			Canvas dropdown = transform.GetComponentInChildren<Canvas>();
+			if (dropdown != null) {
+				dropdown.transform.SetParent(this.transform.root);
+				RectTransform ownerRectTransform = this.GetComponent<RectTransform>();
+				RectTransform dropdownRectTransform = dropdown.GetComponent<RectTransform>();
+				if (ownerRectTransform != null && dropdownRectTransform != null) {
+					//dropdownRectTransform.anchorMin = new Vector2(0f, ownerRectTransform.anchorMin.x);
+					dropdownRectTransform.offsetMax = new Vector2(dropdownRectTransform.offsetMax.x, ownerRectTransform.anchorMin.x);
+				}
+			}
+		}
+	}
+}
