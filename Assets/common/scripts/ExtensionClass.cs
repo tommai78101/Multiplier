8682ecd30619f2fe899088d713275d75d9cff5c4
Thompson Lee <tom.mai78101@gmail.com>
2015-12-04 00:41:25 -0500

    I swear to God, I hoped this UI dropdown is fixed.

--- Assets/common/scripts/ExtensionClass.cs
+++ Assets/common/scripts/ExtensionClass.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 
 namespace Extension {
@@ -22,6 +23,75 @@ namespace Extension {
 				}
 			}
 			return false;
+		}
+
+		public static void AnchorToCorners(this RectTransform transform) {
+			if (transform == null)
+				throw new ArgumentNullException("There is no transform being referenced.");
+
+			if (transform.parent == null)
+				return;
+
+			var parent = transform.parent.GetComponent<RectTransform>();
+
+			Vector2 newAnchorsMin = new Vector2(transform.anchorMin.x + transform.offsetMin.x / parent.rect.width,
+							  transform.anchorMin.y + transform.offsetMin.y / parent.rect.height);
+
+			Vector2 newAnchorsMax = new Vector2(transform.anchorMax.x + transform.offsetMax.x / parent.rect.width,
+							  transform.anchorMax.y + transform.offsetMax.y / parent.rect.height);
+
+			transform.anchorMin = newAnchorsMin;
+			transform.anchorMax = newAnchorsMax;
+			transform.offsetMin = transform.offsetMax = new Vector2(0, 0);
+		}
+
+		public static void SetPivotAndAnchors(this RectTransform trans, Vector2 aVec) {
+			trans.pivot = aVec;
+			trans.anchorMin = aVec;
+			trans.anchorMax = aVec;
+		}
+
+		public static Vector2 GetSize(this RectTransform trans) {
+			return trans.rect.size;
+		}
+
+		public static float GetWidth(this RectTransform trans) {
+			return trans.rect.width;
+		}
+
+		public static float GetHeight(this RectTransform trans) {
+			return trans.rect.height;
+		}
+
+		public static void SetSize(this RectTransform trans, Vector2 newSize) {
+			Vector2 oldSize = trans.rect.size;
+			Vector2 deltaSize = newSize - oldSize;
+			trans.offsetMin = trans.offsetMin - new Vector2(deltaSize.x * trans.pivot.x, deltaSize.y * trans.pivot.y);
+			trans.offsetMax = trans.offsetMax + new Vector2(deltaSize.x * (1f - trans.pivot.x), deltaSize.y * (1f - trans.pivot.y));
+		}
+
+		public static void SetWidth(this RectTransform trans, float newSize) {
+			SetSize(trans, new Vector2(newSize, trans.rect.size.y));
+		}
+
+		public static void SetHeight(this RectTransform trans, float newSize) {
+			SetSize(trans, new Vector2(trans.rect.size.x, newSize));
+		}
+
+		public static void SetBottomLeftPosition(this RectTransform trans, Vector2 newPos) {
+			trans.localPosition = new Vector3(newPos.x + (trans.pivot.x * trans.rect.width), newPos.y + (trans.pivot.y * trans.rect.height), trans.localPosition.z);
+		}
+
+		public static void SetTopLeftPosition(this RectTransform trans, Vector2 newPos) {
+			trans.localPosition = new Vector3(newPos.x + (trans.pivot.x * trans.rect.width), newPos.y - ((1f - trans.pivot.y) * trans.rect.height), trans.localPosition.z);
+		}
+
+		public static void SetBottomRightPosition(this RectTransform trans, Vector2 newPos) {
+			trans.localPosition = new Vector3(newPos.x - ((1f - trans.pivot.x) * trans.rect.width), newPos.y + (trans.pivot.y * trans.rect.height), trans.localPosition.z);
+		}
+
+		public static void SetRightTopPosition(this RectTransform trans, Vector2 newPos) {
+			trans.localPosition = new Vector3(newPos.x - ((1f - trans.pivot.x) * trans.rect.width), newPos.y - ((1f - trans.pivot.y) * trans.rect.height), trans.localPosition.z);
 		}
 	}
 }
--- Assets/singleplayer/ui/DropdownFix.cs
+++ Assets/singleplayer/ui/DropdownFix.cs
@@ -2,20 +2,26 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
+using Extension;
 
 namespace SinglePlayer {
 	public class DropdownFix : Dropdown, IPointerClickHandler {
 		public override void OnPointerClick(PointerEventData eventData) {
 			base.OnPointerClick(eventData);
 
-			Canvas dropdown = transform.GetComponentInChildren<Canvas>();
+			Dropdown dropdown = transform.GetComponentInChildren<Dropdown>();
 			if (dropdown != null) {
-				dropdown.transform.SetParent(this.transform.root);
-				RectTransform ownerRectTransform = this.GetComponent<RectTransform>();
-				RectTransform dropdownRectTransform = dropdown.GetComponent<RectTransform>();
-				if (ownerRectTransform != null && dropdownRectTransform != null) {
-					//dropdownRectTransform.anchorMin = new Vector2(0f, ownerRectTransform.anchorMin.x);
-					dropdownRectTransform.offsetMax = new Vector2(dropdownRectTransform.offsetMax.x, ownerRectTransform.anchorMin.x);
+				Canvas dropdownMenu = dropdown.GetComponentInChildren<Canvas>();
+				if (dropdownMenu != null) {
+					//dropdownMenu.transform.SetParent(this.transform.root);
+					RectTransform ownerRectTransform = dropdown.GetComponent<RectTransform>();
+					RectTransform dropdownRectTransform = dropdownMenu.GetComponent<RectTransform>();
+					if (ownerRectTransform != null && dropdownRectTransform != null) {
+						float height = dropdownRectTransform.GetHeight();
+						float ownerHeight = ownerRectTransform.GetHeight();
+						dropdownRectTransform.offsetMax = new Vector2(dropdownRectTransform.offsetMax.x, -ownerHeight);
+						dropdownRectTransform.offsetMin = new Vector2(dropdownRectTransform.offsetMin.x, dropdownRectTransform.offsetMin.y - height);
+					}
 				}
 			}
 		}
