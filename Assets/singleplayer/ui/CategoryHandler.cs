c3f6565bd1eb026b7bf6cc4177030322e1e39e4d
asperatology <tom.mai78101@gmail.com>
2015-12-09 00:17:11 -0500

    IMCOMPLETE!! Continue working on the category handler.

--- Assets/singleplayer/ui/CategoryHandler.cs
+++ Assets/singleplayer/ui/CategoryHandler.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace SinglePlayer.UI {
+
 	public class CategoryHandler : MonoBehaviour {
+		public GameObject categoryItemPrefab;
+		public List<string> items = new List<string>() {
+			"Health", "Attack", "Speed", "Attack Cooldown", "Split Time", "Merge Time"
+		};
 
-		// Use this for initialization
-		void Start() {
-
-		}
-
-		// Update is called once per frame
-		void Update() {
-
+		public void Start() {
+			if (this.transform.childCount > 0) {
+				for (int i = 0; i < this.transform.childCount; i++) {
+					GameObject.Destroy(this.transform.GetChild(i).gameObject);
+				}
+			}
+			if (this.categoryItemPrefab != null) {
+				for (int i = 0; i < this.items.Count; i++) {
+					GameObject obj = GameObject.Instantiate(this.categoryItemPrefab) as GameObject;
+					Text text = obj.GetComponentInChildren<Text>();
+					text.text = this.items[i];
+					Toggle toggle = obj.GetComponent<Toggle>();
+					if (toggle != null) {
+						if (i == 0) {
+							toggle.isOn = true;
+						}
+						else {
+							toggle.isOn = false;
+						}
+					}
+				}
+			}
 		}
 	}
 }
--- Assets/singleplayer/ui/DifficultyGroup.cs
+++ Assets/singleplayer/ui/DifficultyGroup.cs
@@ -4,32 +4,45 @@ using System.Collections;
 
 namespace SinglePlayer.UI {
 	public class DifficultyGroup : MonoBehaviour {
-		public Toggle easyDifficulty;
-		public Toggle normalDifficulty;
-		public Toggle hardDifficulty;
-		private ToggleGroup difficultyModeGroup;
+		public ToggleGroup difficultyModeGroup;
 
 		// Use this for initialization
 		void Start() {
-			this.easyDifficulty.isOn = false;
-			this.normalDifficulty.isOn = true;
-			this.hardDifficulty.isOn = false;
+			if (this.difficultyModeGroup == null) {
+				GameObject[] menu = GameObject.FindGameObjectsWithTag("Menu");
+				if (menu != null && menu.Length > 0) {
+					foreach (GameObject obj in menu) {
+						if (obj.GetComponent<DifficultyGroup>() != null) {
+							this.difficultyModeGroup = obj.GetComponent<ToggleGroup>();
+							break;
+						}
+					}
+				}
+				if (this.difficultyModeGroup == null) {
+					Debug.LogError("Toggle group isn't set. Please check.");
+				}
+			}
 		}
 
 		public Difficulty GetDifficulty() {
-			foreach (Toggle result in this.difficultyModeGroup.ActiveToggles()) {
-				if (result.isOn) {
-					Text text = result.GetComponentInChildren<Text>();
-					if (text.text.Equals("Easy")) {
-						return Difficulty.Easy;
-					}
-					else if (text.text.Equals("Normal")) {
-						return Difficulty.Normal;
-					}
-					else if (text.text.Equals("Hard")) {
-						return Difficulty.Hard;
+			try {
+				foreach (Toggle result in this.difficultyModeGroup.ActiveToggles()) {
+					if (result.isOn) {
+						Text text = result.GetComponentInChildren<Text>();
+						if (text.text.Equals("Easy")) {
+							return Difficulty.Easy;
+						}
+						else if (text.text.Equals("Normal")) {
+							return Difficulty.Normal;
+						}
+						else if (text.text.Equals("Hard")) {
+							return Difficulty.Hard;
+						}
 					}
 				}
+			}
+			catch {
+				return Difficulty.Normal;
 			}
 			return Difficulty.Normal;
 		}
