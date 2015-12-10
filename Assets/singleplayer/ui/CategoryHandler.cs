146166e518e7b7b257dfdbe91a3b7836bb68f95e
asperatology <tom.mai78101@gmail.com>
2015-12-09 21:07:05 -0500

    I am finally happy with this change.

--- Assets/singleplayer/ui/CategoryHandler.cs
+++ Assets/singleplayer/ui/CategoryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using Extension;
@@ -9,8 +10,11 @@ namespace SinglePlayer.UI {
 		public GameObject categoryItemPrefab;
 		public List<string> items;
 		public ToggleGroup categoriesToggleGroup;
+		public string selectedToggle;
+		private bool flipFlag;
 
 		public void Start() {
+			this.flipFlag = true;
 			this.items = new List<string>() { "Health", "Attack", "Speed", "AtkCooldown", "Split", "Merge" };
 			if (this.categoriesToggleGroup == null) {
 				Debug.LogError("Something is wrong with the toggle group. Please check.");
@@ -29,24 +33,39 @@ namespace SinglePlayer.UI {
 						rectTransform.localScale = Vector3.one;
 						rectTransform.offsetMax = new Vector2(rectTransform.GetWidth() + 20f, rectTransform.offsetMax.y);
 					}
-					Toggle toggle = obj.GetComponent<Toggle>();
-					if (toggle != null) {
-						toggle.group = this.categoriesToggleGroup;
-						if (i == 0) {
-							toggle.isOn = true;
-						}
-						else {
-							toggle.isOn = false;
-						}
-					}
 					Text text = obj.GetComponentInChildren<Text>();
 					if (text != null) {
 						text.text = this.items[i];
 						RectTransform textTransform = text.GetComponent<RectTransform>();
 						if (textTransform != null) {
-							textTransform.offsetMin = new Vector2(-textTransform.GetWidth()+17f, textTransform.offsetMin.y);
+							textTransform.offsetMin = new Vector2(-textTransform.GetWidth() + 17f, textTransform.offsetMin.y);
 						}
 					}
+					Toggle toggle = obj.GetComponent<Toggle>();
+					if (toggle != null) {
+						toggle.group = this.categoriesToggleGroup;
+						if (i == 0) {
+							toggle.isOn = true;
+							this.flipFlag = false;
+							this.OnChangeCategory(toggle);
+						}
+						else {
+							toggle.isOn = false;
+						}
+						toggle.onValueChanged.AddListener(delegate {
+							OnChangeCategory(toggle);
+						});
+					}
+				}
+			}
+		}
+
+		public void OnChangeCategory(Toggle toggle) {
+			this.flipFlag = !this.flipFlag;
+			if (this.flipFlag) {
+				Text text = toggle.GetComponentInChildren<Text>();
+				if (text != null) {
+					this.selectedToggle = text.text;
 				}
 			}
 		}
