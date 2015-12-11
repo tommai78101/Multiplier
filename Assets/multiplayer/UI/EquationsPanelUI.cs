e0cf8e894032b9710c241bb3a4df124f677243b0
asdas <asdasd>
2015-12-11 01:50:27 -0500

    Canvas stuff for Multiplayer.

---
+++ Assets/multiplayer/UI/EquationsPanelUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MultiPlayer {
+	public class EquationsPanelUI : MonoBehaviour {
+
+		// Use this for initialization
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
