cd8b718d596cc31ae780fde094e37e7b4f753466
asperatology <tom.mai78101@gmail.com>
2015-12-21 17:04:37 -0500

    Tweaked the UI to be more consistent.
    Added UI Initializer to prevent the contents of the scroll view to be lopsided outside of the boundaries.

---
+++ Assets/singleplayer/ui/UIInitializer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIInitializer : MonoBehaviour {
+	void Start() {
+		RectTransform trans = this.GetComponent<RectTransform>();
+		if (trans != null) {
+			trans.localPosition = new Vector2(0f, 0f);
+		}
+	}
+}
