4624537a3b534fc3f69c641c10d1353f260434c9
asperatology <tom.mai78101@gmail.com>
2016-01-11 16:29:40 -0500

    Tried again.
    Fixed Unity 5.2.3f1 vs. Unity 5.3.1f1 incompatibility.
    
    Also, blocked WebGL related code.

--- Assets/menu/Editor/RainbowEffect.cs
+++ Assets/menu/Editor/RainbowEffect.cs
@@ -15,7 +15,12 @@ public class RainbowEffect : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.degrees = (this.degrees + Time.deltaTime * 50f) % 360f;
+#if UNITY_5_2_3
+		this.textShadow.effectColor = EditorGUIUtility.HSVToRGB(degrees / 360f, 1f, 1f);
+		this.text.color = EditorGUIUtility.HSVToRGB(((degrees + 180f) % 360f) / 360f, 1f, 1f);
+#else
 		this.textShadow.effectColor = Color.HSVToRGB(degrees / 360f, 1f, 1f);
 		this.text.color = Color.HSVToRGB(((degrees + 180f) % 360f) / 360f, 1f, 1f);
+#endif
 	}
 }
