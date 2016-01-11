20bba2a9a1c157344123a7dd0fc3b8d6c873ba7d
asperatology <tom.mai78101@gmail.com>
2016-01-11 18:17:55 -0500

    Can now toggle pause menu by pressing "Unpause" button.

--- Assets/singleplayer/scripts/SingleHost.cs
+++ Assets/singleplayer/scripts/SingleHost.cs
@@ -26,14 +26,7 @@ public class SingleHost : NetworkManager {
 	public void Update() {
 		//TODO: Create a menu to back out into the main menu.
 		if (this.enablePauseGameMenu && Input.GetKeyUp(KeyCode.Escape)) {
-			if (this.pauseMenuGroup != null) {
-				if (this.pauseMenuGroup.gameObject.activeInHierarchy || this.pauseMenuGroup.gameObject.activeSelf) {
-					this.pauseMenuGroup.gameObject.SetActive(false);
-				}
-				else {
-					this.pauseMenuGroup.gameObject.SetActive(true);
-				}
-			}
+			TogglePauseMenu();
 		}
 
 		if (this.notReady) {
@@ -158,5 +151,16 @@ public class SingleHost : NetworkManager {
 
 		this.enablePauseGameMenu = false;
 		this.notReady = true;
+	}
+
+	public void TogglePauseMenu() {
+		if (this.pauseMenuGroup != null) {
+			if (this.pauseMenuGroup.gameObject.activeInHierarchy || this.pauseMenuGroup.gameObject.activeSelf) {
+				this.pauseMenuGroup.gameObject.SetActive(false);
+			}
+			else {
+				this.pauseMenuGroup.gameObject.SetActive(true);
+			}
+		}
 	}
 }
