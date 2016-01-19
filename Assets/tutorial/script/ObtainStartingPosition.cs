4fae53a3b040331bb3096f2aef7cd124397b41c5
asperatology <tom.mai78101@gmail.com>
2016-01-18 20:07:29 -0500

    CURSOR ANIMATIONS!!
    Useful for tutorials.

---
+++ Assets/tutorial/script/ObtainStartingPosition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tutorial {
+	public class ObtainStartingPosition : MonoBehaviour {
+		public Cursor parentCursor;
+		public RectTransform rectTransform;
+
+		public void Start() {
+			this.rectTransform = this.GetComponent<RectTransform>();
+			if (this.rectTransform == null) {
+				Debug.LogError("Cannot obtain RectTransform. Please check.");
+			}
+		}
+
+		public void Update() {
+			this.parentCursor.startingPosition = this.rectTransform.localPosition;
+	    }
+	}
+}
