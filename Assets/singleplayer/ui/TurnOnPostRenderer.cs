07892f377446f2dd20ce9d11e894f257a2e0c8ca
asperatology <tom.mai78101@gmail.com>
2016-01-20 19:45:37 -0500

    OHHH
    I completely forgot to add all of the changed files...
    
    No wonder...
    
    _____
    
    Fixed global manager not being able to set max value after 2 when the slider has changed in the UI panel.

---
+++ Assets/singleplayer/ui/TurnOnPostRenderer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Common;
+
+namespace SinglePlayer {
+	public class TurnOnPostRenderer : MonoBehaviour {
+		public PostRenderer postRenderer;
+		public bool isInitialized;
+
+		public void Start() {
+			this.isInitialized = false;
+			this.postRenderer = this.GetComponent<PostRenderer>();
+			if (this.postRenderer == null) {
+				Debug.Log("Can't initialize post renderer. Moving to Update() to check.");
+			}
+		}
+
+		// Update is called once per frame
+		public void Update() {
+			if (!this.isInitialized) {
+				if (this.postRenderer == null) {
+					this.postRenderer = this.GetComponent<PostRenderer>();
+					if (this.postRenderer != null) {
+						this.postRenderer.enabled = false;
+					}
+				}
+			}
+		}
+
+		public void TurnOnSelectionBox() {
+			if (this.postRenderer != null) {
+				this.postRenderer.enabled = true;
+				this.isInitialized = true;
+				this.enabled = false;
+			}
+		}
+	}
+}
