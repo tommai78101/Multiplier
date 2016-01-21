ce1d6f5e28f54a64a36b85e794ce4de1775706a8
asperatology <tom.mai78101@gmail.com>
2016-01-20 19:57:11 -0500

    Made the AI Manager to check for max unit count.

--- Assets/singleplayer/ui/GlobalManager.cs
+++ Assets/singleplayer/ui/GlobalManager.cs
@@ -4,9 +4,11 @@ using System.Collections;
 namespace SinglePlayer {
 	public class GlobalManager : MonoBehaviour {
 		public int playerMaxUnitCount;
+		public int aiMaxUnitCount;
 
 		public void Start() {
 			this.playerMaxUnitCount = 1;
+			this.aiMaxUnitCount = 1;
 		}
 	}
 }
