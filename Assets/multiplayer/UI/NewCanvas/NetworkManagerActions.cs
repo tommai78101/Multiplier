15a4969ae0c50fa8a44dfc9a5026f7de96fae654
tom.mai78101 <tom.mai78101@gmail.com>
2016-02-09 13:36:03 -0500

    Adding new multiplayer UI.

---
+++ Assets/multiplayer/UI/NewCanvas/NetworkManagerActions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkManagerActions : MonoBehaviour {
+	public NetworkManager networkManager;
+
+	public void Start() {
+		if (this.networkManager == null) {
+			GameObject obj = GameObject.FindGameObjectWithTag("NetworkManager");
+			this.networkManager = obj.GetComponent<NetworkManager>();
+			if (this.networkManager == null) {
+				Debug.LogError("Cannot find Network Manager.");
+			}
+		}
+	}
+
+	public void StartLANHost() {
+		this.networkManager.StartHost();
+	}
+	public void StartLANClient() {
+		this.networkManager.StartHost();
+	}
+	public void StartMatchMaker() {
+		this.networkManager.StartHost();
+	}
+}
