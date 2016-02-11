b33f5c2d96d115227e9db85bf97775358af3887f
tlee2 <tlee2@wpi.edu>
2016-02-11 18:19:09 -0500

    Added paste from clipboard.

---
+++ Assets/multiplayer/UI/NewCanvas/PasteFromClipboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+public class PasteFromClipboard : MonoBehaviour {
+	public InputField inputField;
+
+	public void OnClick() {
+		if (inputField != null) {
+			PasteIPAddress(EditorGUIUtility.systemCopyBuffer);
+		}
+	}
+
+
+	public void PasteIPAddress(string buffer) {
+		this.inputField.text = buffer;
+		Text text = this.inputField.placeholder.GetComponent<Text>();
+		if (text != null) {
+			text.text = buffer;
+		}
+		text = this.inputField.GetComponent<Text>();
+		if (text != null) {
+			text.text = buffer;
+		}
+	}
+}
