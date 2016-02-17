f8131d6368d6688d8301056de071965c1e152268
asperatology <tom.mai78101@gmail.com>
2016-02-16 21:35:40 -0500

    Reversing iteration.

--- Assets/new_multiplayer/NewSpawner.cs
+++ Assets/new_multiplayer/NewSpawner.cs
@@ -493,7 +493,8 @@ namespace MultiPlayer {
 		}
 
 		private void SelectObjectsInRect() {
-			foreach (NewUnitStruct temp in this.unitList) {
+			for (int i = this.unitList.Count - 1; i > 0; i--) {
+				NewUnitStruct temp = this.unitList[i];
 				GameObject obj = temp.unit.gameObject;
 				if (obj == null) {
 					this.unitList.Remove(temp);
