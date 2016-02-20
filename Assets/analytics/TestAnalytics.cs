b24388a19b63b4be21d232575c5978c0b6afe937
asperatology <tom.mai78101@gmail.com>
2016-02-20 16:11:28 -0500

    Removing Unity Analytics for now. It's bugging me. And it's not used right now.

--- Assets/analytics/TestAnalytics.cs
+++ Assets/analytics/TestAnalytics.cs
@@ -1,31 +1,31 @@
-﻿#if (UNITY_5_2_3 || UNITY_5_2_4)
-#else
-using UnityEngine;
-using UnityEngine.Analytics;
-using System.Collections.Generic;
+﻿//#if (UNITY_5_2_3 || UNITY_5_2_4)
+//#else
+//using UnityEngine;
+//using UnityEngine.Analytics;
+//using System.Collections.Generic;
 
-public class TestAnalytics : MonoBehaviour {
-	IDictionary<string, object> customDictionary = new Dictionary<string, object>();
+//public class TestAnalytics : MonoBehaviour {
+//	IDictionary<string, object> customDictionary = new Dictionary<string, object>();
 
-	void Start() {
-		this.customDictionary.Add("Testing Event Message", "Hello world. And it is truly a 'Hello world' message.");
-	}
+//	void Start() {
+//		this.customDictionary.Add("Testing Event Message", "Hello world. And it is truly a 'Hello world' message.");
+//	}
 
-	// Update is called once per frame
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.P)) {
-			//Analytics.CustomEvent("Hello World Event", this.customDictionary);
+//	// Update is called once per frame
+//	void Update() {
+//		if (Input.GetKeyDown(KeyCode.P)) {
+//			//Analytics.CustomEvent("Hello World Event", this.customDictionary);
 
-			//Analytics.Transaction("12345abcde", 0.99m, "USD", null, null);
+//			//Analytics.Transaction("12345abcde", 0.99m, "USD", null, null);
 
-			Gender gender = Gender.Female;
-			Analytics.SetUserGender(gender);
+//			Gender gender = Gender.Female;
+//			Analytics.SetUserGender(gender);
 
-			int birthYear = 2014;
-			Analytics.SetUserBirthYear(birthYear);
+//			int birthYear = 2014;
+//			Analytics.SetUserBirthYear(birthYear);
 
-			Analytics.SetUserId("Test11234");
-		}
-	}
-}
-#endif
+//			Analytics.SetUserId("Test11234");
+//		}
+//	}
+//}
+//#endif
