8a738602ec12d2c9c3f06e3aa240800dd6482fb4
tlee2 <tlee2@wpi.edu>
2016-02-22 01:07:18 -0500

    Forgot to add the parameters.

--- Assets/new_multiplayer/NewGameUnit.cs
+++ Assets/new_multiplayer/NewGameUnit.cs
@@ -114,7 +114,7 @@ namespace MultiPlayer {
 				updateProperties(changes);
 			}
 			else {
-				CmdDestroy(this.gameObject);
+				CmdDestroy(this.gameObject, this.properties.targetUnit);
 			}
 		}
 
