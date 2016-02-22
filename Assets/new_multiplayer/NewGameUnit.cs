58d2355fec88a602887670bb35b91191210ef309
tlee2 <tlee2@wpi.edu>
2016-02-22 00:52:45 -0500

    Added recovery and attacking to NewGameUnit. I hoped everything works out fine.

--- Assets/new_multiplayer/NewGameUnit.cs
+++ Assets/new_multiplayer/NewGameUnit.cs
@@ -10,6 +10,8 @@ namespace MultiPlayer {
 		public bool isMerging;
 		public bool isSelected;
 		public bool isCommanded;
+		public bool isAttackCooldownEnabled;
+		public bool isRecoveryEnabled;
 		public int currentHealth;
 		public int maxHealth;
 		public int level;
@@ -27,6 +29,8 @@ namespace MultiPlayer {
 		public bool isMerging;
 		public bool isSelected;
 		public bool isCommanded;
+		public bool isAttackCooldownEnabled;
+		public bool isRecoveryEnabled;
 		public int damage;
 		public int newLevel;
 		public Vector3 mousePosition;
@@ -56,8 +60,10 @@ namespace MultiPlayer {
 		public delegate void UpdateProperties(NewChanges changes);
 		public UpdateProperties updateProperties;
 		public NavMeshAgent agent;
-
 		public GameObject selectionRing;
+
+		private float recoveryCounter;
+		private float attackCooldownCounter;
 
 		public void Start() {
 			this.properties = new UnitProperties();
@@ -86,6 +92,8 @@ namespace MultiPlayer {
 			if (this.agent == null) {
 				Debug.LogError("Cannot obtain nav mesh agent from game unit.");
 			}
+			this.recoveryCounter = 0f;
+			this.attackCooldownCounter = 0f;
 		}
 
 		[Command]
@@ -113,42 +121,20 @@ namespace MultiPlayer {
 				return;
 			}
 
-			//if (Input.GetKeyUp(KeyCode.L)) {
-			//	Debug.Log("Damage time!");
-			//	CmdTakeDamage(1);
-			//}
-
-			if (this.properties.isCommanded) {
-				if (this.properties.mouseTargetPosition != this.properties.oldMouseTargetPosition) {
-					CmdSetDestination(this.gameObject, this.properties.mouseTargetPosition);
-				}
-			}
-			else {
-				if (this.agent.ReachedDestination()) {
-					if (this.properties.isCommanded) {
-						NewChanges changes = this.CurrentProperty();
-						changes.isCommanded = false;
-						this.CmdUpdateProperty(this.gameObject, changes);
-					}
-					if (!this.properties.isCommanded) {
-						CmdSetDestination(this.gameObject, this.properties.enemyTargetPosition);
-					}
-				}
-			}
-
-			if (this.properties.isSelected) {
-				this.selectionRing.SetActive(true);
-			}
-			else {
-				this.selectionRing.SetActive(false);
-			}
+			HandleMovement();
+			HandleSelectionRing();
+			HandleAttacking();
+			HandleRecovering();
+			HandleStatus();
 		}
 
 		public void NewProperty(NewChanges changes) {
 			UnitProperties pro = new UnitProperties();
 			pro = this.properties;
-			if (changes.damage > 0) {
+			if (changes.damage > 0 && this.attackCooldownCounter <= 0f) {
 				pro.currentHealth -= changes.damage;
+				pro.isAttackCooldownEnabled = true;
+				this.attackCooldownCounter = 1f;
 			}
 			if (pro.mouseTargetPosition != changes.mousePosition) {
 				pro.oldMouseTargetPosition = pro.mouseTargetPosition;
@@ -163,6 +149,12 @@ namespace MultiPlayer {
 			pro.isMerging = changes.isMerging;
 			pro.level = changes.newLevel;
 			pro.isCommanded = changes.isCommanded;
+			if (changes.isRecoveryEnabled) {
+				pro.isRecoveryEnabled = changes.isRecoveryEnabled;
+				if (this.recoveryCounter <= 0f) {
+					this.recoveryCounter = 1f;
+				}
+			}
 			OnPropertiesChanged(pro);
 		}
 
@@ -187,6 +179,67 @@ namespace MultiPlayer {
 			this.CmdUpdateProperty(this.gameObject, changes);
 		}
 
+		//*** ----------------------------   PRIVATE METHODS  -------------------------
+
+		private void HandleMovement() {
+			if (this.properties.isCommanded) {
+				if (this.properties.mouseTargetPosition != this.properties.oldMouseTargetPosition) {
+					CmdSetDestination(this.gameObject, this.properties.mouseTargetPosition);
+				}
+			}
+			else {
+				if (this.agent.ReachedDestination()) {
+					if (this.properties.isCommanded) {
+						NewChanges changes = this.CurrentProperty();
+						changes.isCommanded = false;
+						this.CmdUpdateProperty(this.gameObject, changes);
+					}
+					if (!this.properties.isCommanded) {
+						CmdSetDestination(this.gameObject, this.properties.enemyTargetPosition);
+					}
+				}
+			}
+		}
+
+		private void HandleSelectionRing() {
+			if (this.properties.isSelected) {
+				this.selectionRing.SetActive(true);
+			}
+			else {
+				this.selectionRing.SetActive(false);
+			}
+		}
+
+		private void HandleAttacking() {
+			if (this.properties.targetUnit != null && this.attackCooldownCounter <= 0f) {
+				CmdAttack(this.gameObject, this.properties.targetUnit, 1);
+			}
+			else if (this.attackCooldownCounter > 0f) {
+				this.attackCooldownCounter -= Time.deltaTime;
+			}
+		}
+
+		private void HandleRecovering() {
+			if (this.properties.isRecoveryEnabled) {
+				if (recoveryCounter > 0f) {
+					recoveryCounter -= Time.deltaTime;
+				}
+				else {
+					NewChanges changes = this.CurrentProperty();
+					changes.isRecoveryEnabled = false;
+					this.NewProperty(changes);
+				}
+			}
+		}
+
+		private void HandleStatus() {
+			if (this.properties.currentHealth <= 0) {
+				CmdDestroy(this.gameObject);
+			}
+		}
+
+		//----------------------------  COMMANDS and CLIENTRPCS  ----------------------------
+
 		[Command]
 		public void CmdDestroy(GameObject obj) {
 			NetworkServer.Destroy(obj);
@@ -197,6 +250,23 @@ namespace MultiPlayer {
 			NewGameUnit unit = obj.GetComponent<NewGameUnit>();
 			if (unit != null) {
 				unit.NewProperty(changes);
+			}
+		}
+
+		[Command]
+		public void CmdAttack(GameObject attacker, GameObject victim, int damage) {
+			NewGameUnit victimUnit = victim.GetComponent<NewGameUnit>();
+			if (victimUnit != null) {
+				NewChanges changes = victimUnit.CurrentProperty();
+				changes.damage = damage;
+				changes.isRecoveryEnabled = true;
+				victimUnit.NewProperty(changes);
+			}
+			NewGameUnit attackerUnit = attacker.GetComponent<NewGameUnit>();
+			if (attackerUnit != null) {
+				NewChanges changes = attackerUnit.CurrentProperty();
+				changes.isAttackCooldownEnabled = true;
+				attackerUnit.NewProperty(changes);
 			}
 		}
 
