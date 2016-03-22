35542eb6cf7f325eb22f1b5826ed0bd37a7772d3
tlee2 <tlee2@wpi.edu>
2016-03-22 14:27:10 -0400

    Added a pause menu.

---
+++ Assets/simulation/scripts/SimulationPauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SimulationPauseMenu : MonoBehaviour {
+	public KeyCode triggerKey;
+	public CanvasGroup canvasGroup;
+
+	public void Start() {
+		if (this.canvasGroup == null) {
+			Debug.LogError("Canvas group is not defined.");
+			return;
+		}
+	}
+
+	public void Update() {
+		if (Input.GetKeyUp(triggerKey)) {
+			Toggle();
+		}
+	}
+
+	public void Toggle() {
+		this.canvasGroup.alpha = (this.canvasGroup.alpha == 1) ? 0 : 1;
+		this.canvasGroup.interactable = (this.canvasGroup.interactable) ? false : true;
+		this.canvasGroup.blocksRaycasts = (this.canvasGroup.blocksRaycasts) ? false : true;
+	}
+}
