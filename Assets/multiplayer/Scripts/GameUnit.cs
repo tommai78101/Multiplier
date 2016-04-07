8554736456becaa051243c61d74b7308edfd3645
tlee2 <tlee2@wpi.edu>
2016-04-07 00:45:35 -0400

    Forgot another crucial unit attributes: Attack Cooldown.

--- Assets/multiplayer/Scripts/GameUnit.cs
+++ Assets/multiplayer/Scripts/GameUnit.cs
@@ -224,7 +224,7 @@ namespace MultiPlayer {
 				this.currentHealth = this.maxHealth;
 				this.attackPower = this.unitAttributes.attackPrefabList[this.level];
 				this.speed = this.unitAttributes.speedPrefabList[this.level];
-				this.attackCooldownCounter = this.unitAttributes.attackCooldownPrefabList[this.level];
+				this.attackCooldown = this.unitAttributes.attackCooldownPrefabList[this.level];
 				NavMeshAgent agent = this.GetComponent<NavMeshAgent>();
 				if (agent != null) {
 					agent.speed = this.speed;
