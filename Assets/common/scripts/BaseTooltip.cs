bbd21ca9d036a41a4aef5e0299ee5a8d9ebc0d89
tlee2 <tlee2@wpi.edu>
2016-04-07 03:27:21 -0400

    Just adding some comments. I deem them to be useful in the future.

--- Assets/common/scripts/BaseTooltip.cs
+++ Assets/common/scripts/BaseTooltip.cs
@@ -24,30 +24,33 @@ public class BaseTooltip : MonoBehaviour {
 	private String textMemory;
 	private bool enableTooltipFlag;
 
+	//Initialization stuffs.
 	public void Start() {
-		this.panel = this.GetComponent<Image>();
-		this.canvasGroup = this.GetComponent<CanvasGroup>();
-		this.rectTransform = this.GetComponent<RectTransform>();
-		this.tooltipTextContent = this.GetComponentInChildren<Text>();
+		this.panel = this.GetComponent<Image>();  //Part of the default Panel UI component.
+		this.canvasGroup = this.GetComponent<CanvasGroup>(); //Used to switch Tooltip to be visible/invisible.
+		this.rectTransform = this.GetComponent<RectTransform>();  //Gets the Panel UI's RectTransform.
+		this.tooltipTextContent = this.GetComponentInChildren<Text>(); //Gets the Text UI component that's the child of this Panel UI.
 		if (this.paddingSize == 0) {
-			this.paddingSize = 10;
+			this.paddingSize = 10; //Sets the text margins on all 4 sides (top, left, bottom, right).
 		}
 		this.transform.position = Vector3.zero;
 		rectTransform.localScale = Vector3.one;
 		rectTransform.localPosition = Vector3.zero;
-		this.tooltipTextContent.text = "";
-		this.enableTooltipFlag = true;
-		this.SetToolTipHidden(true);
+		this.tooltipTextContent.text = "";  //Initialize the actual Tooltip text content to an empty string.
+		this.enableTooltipFlag = true;  //True, if I want to enable the Tooltip. False, otherwise.
+		this.SetToolTipHidden(true); //Toggles visibility using the CanvasGroup.
 	}
 
 	public void Update() {
-		if (Input.GetKeyUp(this.toggleKey) || Input.GetKeyUp(this.alternateToggleKey)) {
+		if (Input.GetKeyUp(this.toggleKey) || Input.GetKeyUp(this.alternateToggleKey)) {  //The toggle keys are LEFT and RIGHT SHIFT.
 			this.enableTooltipFlag = !this.enableTooltipFlag;
 			this.SetToolTipHidden(this.enableTooltipFlag);
 		}
 
 		if (this.panel != null && this.tooltipTextContent != null) {
-			this.tooltipTextContent.text = this.textMemory;
+			this.tooltipTextContent.text = this.textMemory;  //Sets the tooltip text content to the child Text UI component.
+
+			//The following code adjusts the tooltip Panel UI's width and height. There's no other way I know of that auto-adjusts the child Text UI component's text content to fit nicely.
 			if (this.tooltipTextContent.preferredWidth > Screen.width) {
 				this.panel.rectTransform.sizeDelta = new Vector2(this.tooltipTextContent.preferredWidth * 0.5f, this.tooltipTextContent.preferredHeight + this.paddingSize * 2f);
 			}
@@ -56,6 +59,7 @@ public class BaseTooltip : MonoBehaviour {
 			}
 		}
 
+		//If followCursorFlag is TRUE, the tooltip will follow the cursor at a specific offset from the mouse cursor's position.
 		if (this.target != null && this.followCursorFlag) {
 			Vector2 pos = Input.mousePosition;
 			pos.x -= this.xOffset;
@@ -64,10 +68,12 @@ public class BaseTooltip : MonoBehaviour {
 		}
 	}
 
+	//Just setting text to the child Text UI component.
 	public void SetText(string hint) {
 		this.textMemory = hint;
 	}
 
+	//Toggles tooltip visibility/
 	public void SetToolTipHidden(bool flag) {
 		if (this.enableTooltipFlag) {
 			if (flag) {
@@ -88,6 +94,10 @@ public class BaseTooltip : MonoBehaviour {
 		}
 	}
 
+	//This sets the target. Target is a UI component that contains tooltip hint text contents. 
+	//When a mouse cursor enters/exits the target's RectTransform boundaries, this function 
+	//will be called. Its intended purpose is merely just to point out the last touched UI 
+	//component the player has the mouse hovering over.
 	public void SetTarget(RectTransform obj) {
 		this.target = obj;
 	}
